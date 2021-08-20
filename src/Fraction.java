56aba2d90810bac1e9b8022a2400a2f4828004cd
obudima <obudima@gmail.com>
2021-08-20 17:45:36 +0500

    Task 20.08

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,61 +1,120 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Fraction {
     private int numerator;
     private int denomenator;
 
-    public Fraction() {
+    public Fraction() throws EnterTypeException, ZeroDivException {
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.println("Введите числитель:");
+            numerator = in.nextInt();
+            System.out.println("Введите знаменатель:");
+            denomenator = in.nextInt();
+        } catch (InputMismatchException e) {
+            throw new EnterTypeException();
+        }
+        if (denomenator == 0) throw new ZeroDivException();
     }
 
-    public Fraction(int numerator, int denomenator) {
+    public Fraction(int numerator, int denomenator) throws ZeroDivException {
         this.numerator = numerator;
         this.denomenator = denomenator;
+        if (this.denomenator == 0) throw new ZeroDivException();
     }
 
-    public void add(Fraction fraction) {
+    public void add(Fraction fraction) throws ZeroDivException, TypeOverflowException {
+        long checkMax;
+        checkMax = (long) this.numerator * fraction.denomenator + (long) fraction.numerator * this.denomenator;
+        if (checkMax > Integer.MAX_VALUE || checkMax < Integer.MIN_VALUE)
+            throw new TypeOverflowException(String.valueOf(checkMax));
         this.numerator = this.numerator * fraction.denomenator + fraction.numerator * this.denomenator;
+        checkMax = (long) this.denomenator * fraction.denomenator;
+        if (checkMax > Integer.MAX_VALUE || checkMax < Integer.MIN_VALUE)
+            throw new TypeOverflowException(String.valueOf(checkMax));
         this.denomenator *= fraction.denomenator;
         this.reduce();
     }
 
-    public void add(int n) {
+    public void add(int n) throws ZeroDivException, TypeOverflowException {
+        long checkMax;
+        checkMax = (long) this.numerator + (long) n * this.denomenator;
+        if (checkMax > Integer.MAX_VALUE || checkMax < Integer.MIN_VALUE)
+            throw new TypeOverflowException(String.valueOf(checkMax));
         this.numerator += n * this.denomenator;
         this.reduce();
     }
 
-    public void divide(Fraction fraction) {
+    public void divide(Fraction fraction) throws ZeroDivException, TypeOverflowException {
+        long checkMax;
+        checkMax = (long) this.numerator * fraction.denomenator;
+        if (checkMax > Integer.MAX_VALUE || checkMax < Integer.MIN_VALUE)
+            throw new TypeOverflowException(String.valueOf(checkMax));
         int n = fraction.numerator;
         this.numerator = this.numerator * fraction.denomenator;
+        checkMax = (long) this.denomenator * n;
+        if (checkMax > Integer.MAX_VALUE || checkMax < Integer.MIN_VALUE)
+            throw new TypeOverflowException(String.valueOf(checkMax));
         this.denomenator *= n;
         this.reduce();
     }
 
-    public void divide(int n) {
+    public void divide(int n) throws ZeroDivException, TypeOverflowException {
+        long checkMax;
+        checkMax = (long) this.denomenator * n;
+        if (checkMax > Integer.MAX_VALUE || checkMax < Integer.MIN_VALUE)
+            throw new TypeOverflowException(String.valueOf(checkMax));
         this.denomenator *= n;
         this.reduce();
     }
 
-    public void multiply(Fraction fraction) {
+    public void multiply(Fraction fraction) throws ZeroDivException, TypeOverflowException {
+        long checkMax;
+        checkMax = (long) this.numerator * fraction.numerator;
+        if (checkMax > Integer.MAX_VALUE || checkMax < Integer.MIN_VALUE)
+            throw new TypeOverflowException(String.valueOf(checkMax));
         this.numerator *= fraction.numerator;
+        checkMax = (long) this.denomenator * fraction.denomenator;
+        if (checkMax > Integer.MAX_VALUE || checkMax < Integer.MIN_VALUE)
+            throw new TypeOverflowException(String.valueOf(checkMax));
         this.denomenator *= fraction.denomenator;
         this.reduce();
     }
 
-    public void multiply(int n) {
+    public void multiply(int n) throws ZeroDivException, TypeOverflowException {
+        long checkMax;
+        checkMax = (long) this.numerator * n;
+        if (checkMax > Integer.MAX_VALUE || checkMax < Integer.MIN_VALUE)
+            throw new TypeOverflowException(String.valueOf(checkMax));
         this.numerator *= n;
         this.reduce();
     }
 
-    public void subtract(Fraction fraction) {
+    public void subtract(Fraction fraction) throws ZeroDivException, TypeOverflowException {
+        long checkMax;
+        checkMax = (long) this.numerator * fraction.denomenator - (long) fraction.numerator * this.denomenator;
+        if (checkMax > Integer.MAX_VALUE || checkMax < Integer.MIN_VALUE)
+            throw new TypeOverflowException(String.valueOf(checkMax));
         this.numerator = this.numerator * fraction.denomenator - fraction.numerator * this.denomenator;
+
+        checkMax = (long) this.denomenator * fraction.denomenator;
+        if (checkMax > Integer.MAX_VALUE || checkMax < Integer.MIN_VALUE)
+            throw new TypeOverflowException(String.valueOf(checkMax));
         this.denomenator *= fraction.denomenator;
         this.reduce();
     }
 
-    public void subtract(int n) {
+    public void subtract(int n) throws ZeroDivException, TypeOverflowException {
+        long checkMax;
+        checkMax = (long) this.numerator - (long) n * this.denomenator;
+        if (checkMax > Integer.MAX_VALUE || checkMax < Integer.MIN_VALUE)
+            throw new TypeOverflowException(String.valueOf(checkMax));
         this.numerator -= n * this.denomenator;
         this.reduce();
     }
 
-    public void print() {
+    public void print() throws ZeroDivException {
         this.reduce();
         System.out.println(this.toString());
     }
@@ -65,31 +124,57 @@ public class Fraction {
         return strFraction;
     }
 
-    public void nextFraction() {
+    public void nextFraction() throws ZeroDivException {
         this.numerator += 1;
         this.reduce();
     }
 
     private int getGCD(int a, int b) {
-        int gcd = 0;
+        int gcd = 1;
         for (int i = 1; i <= a && i <= b; i++) {
             if (a % i == 0 && b % i == 0) gcd = i;
         }
         return gcd;
     }
 
-    private void reduce() {
+    private void reduce() throws ZeroDivException {
         int gcd = getGCD(Math.abs(numerator), Math.abs(denomenator));
-        if (gcd != 0) {
+        try {
             numerator /= gcd;
             denomenator /= gcd;
+        } catch (ArithmeticException e) {
+            throw new ZeroDivException(e.getMessage());
         }
         if (denomenator < 0) {
             numerator *= -1;
             denomenator *= -1;
         }
+        if (denomenator == 0) throw new ZeroDivException();
+    }
+
+    public static class ZeroDivException extends Exception {
+        public ZeroDivException() {
+            super("Ошибка деления на 0!");
+        }
+
+        public ZeroDivException(String msg) {
+            super(msg);
+        }
+    }
+
+    public static class TypeOverflowException extends Exception {
+        public TypeOverflowException() {
+            super("Ошибка обработки значения!");
+        }
+
+        public TypeOverflowException(String msg) {
+            super("Ошибка обработки значения (" + msg + ")!");
+        }
+    }
+
+    public static class EnterTypeException extends Exception {
+        public EnterTypeException() {
+            super("Ошибка при вводе значения!");
+        }
     }
 }
-
-
-
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,57 @@
+import com.sun.source.tree.TryTree;
+
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        //Task1.Task1();
-        //Task2.Task2();
-        //Task3.Task3();
+        try {
+            Fraction fraction0 = new Fraction(500, 0);
+            fraction0.print();
+        }
+        catch (Fraction.ZeroDivException e)
+        {
+            System.out.println("Ошибка деления на ноль!");
+            e.printStackTrace();
+        }
 
-        Fraction fraction1 = new Fraction(-5, 6);
-        Fraction fraction2 = new Fraction(2, -3);
-        fraction1.divide(fraction2);
-        fraction1.print();
+        try {
+            Fraction fraction1 = new Fraction(0, 1);
+            Fraction fraction2 = new Fraction(0, 1);
+            Fraction fraction3 = new Fraction(99999999, 9999999);
+            Fraction fraction4 = new Fraction(99999999, 99999999);
+
+            fraction1.add(1);
+            fraction1.print();
+            fraction2.print();
+            fraction1.multiply(fraction2);
+            fraction1.print();
+            fraction1.subtract(1);
+            fraction1.print();
+            fraction3.multiply(fraction4);
+            fraction3.print();
+            fraction1.divide(fraction2);
+            fraction1.print();
+        } catch (Fraction.ZeroDivException e) {
+            System.out.println("Ошибка деления на ноль!");
+            e.printStackTrace();
+        }
+        catch (Fraction.TypeOverflowException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            Fraction fraction5 = new Fraction();
+            fraction5.print();
+        }
+        catch (Fraction.ZeroDivException e) {
+            System.out.println("Ошибка деления на ноль!");
+            e.printStackTrace();
+        }
+        catch (Fraction.EnterTypeException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
     }
 }
